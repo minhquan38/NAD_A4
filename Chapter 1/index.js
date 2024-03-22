043bee5812c716fecdaac7bf0bfacb33c3bf978f
Quan Khuong <vkhuong2363@conestogac.on.ca>
2024-03-22 12:27:35 -0400

    Learn methods of calling AJAX

--- Chapter 1/index.js
+++ Chapter 1/index.js
@@ -21,3 +21,35 @@ document.addEventListener("scroll", () => {
   const positionY = window.scrollY;
   console.log(positionY);
 });
+
+const APIUrl = "https://swapi.dev/api/people/";
+
+// JQuery AJAX method
+$.ajax({
+  type: "GET",
+  url: APIUrl,
+  success: function (response) {
+    console.log("ajax", response);
+  },
+  error: function (error) {
+    console.log(error);
+  },
+});
+
+// XMLHttoRequest
+
+const req = new XMLHttpRequest();
+req.addEventListener("readystatechange", () => {
+  if (req.readyState === 4) {
+    console.log("XMLHttp", JSON.parse(req.responseText));
+  }
+});
+
+req.open("GET", APIUrl);
+req.send();
+
+// Fetch
+fetch(APIUrl)
+  .then((response) => response.json())
+  .then((data) => console.log("fetch", data))
+  .catch((err) => console.log(err));
