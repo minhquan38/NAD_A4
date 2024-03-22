ff65572f9db35c5b1e2dac80446aa300ed21281f
Quan Khuong <vkhuong2363@conestogac.on.ca>
2024-03-22 12:10:16 -0400

    Use event handler in JS

--- Chapter 1/index.js
+++ Chapter 1/index.js
@@ -3,3 +3,21 @@ console.log("Hello world");
 const testEL = document.getElementById("test-el");
 
 testEL.textContent = "Bye Bye";
+
+testEL.addEventListener("click", () => {
+  console.log("click");
+  testEL.innerHTML = "<b>Clicked</b>";
+});
+
+testEL.addEventListener("mouseover", () => {
+  console.log("on");
+});
+
+testEL.addEventListener("mouseout", () => {
+  console.log("off");
+});
+
+document.addEventListener("scroll", () => {
+  const positionY = window.scrollY;
+  console.log(positionY);
+});
