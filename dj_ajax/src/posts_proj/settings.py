952d0dd585a1c70788dee5a185b862f1d67eba94
Quan Khuong <vkhuong2363@conestogac.on.ca>
2024-03-26 20:37:02 -0400

    Create the load more for posts cards

--- dj_ajax/src/posts_proj/settings.py
+++ dj_ajax/src/posts_proj/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = [
-  BASE_DIR /'static',
-  BASE_DIR /'posts' / 'static',
-  BASE_DIR / 'profiles' / 'static'
+STATICFILES_DIRS = [
+  BASE_DIR / 'static',
+  BASE_DIR / 'posts/static',
+  BASE_DIR / 'profiles/static',
 ]
 
 MEDIA_URL='/media/'
