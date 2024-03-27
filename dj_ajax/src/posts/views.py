f42756d559cf8eb85a888c5d6055e9c6379a3909
Quan Khuong <vkhuong2363@conestogac.on.ca>
2024-03-26 22:03:23 -0400

    Create the text fields for the modal

--- dj_ajax/src/posts/views.py
+++ dj_ajax/src/posts/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse 
+from .forms import PostForm
+from profiles.models import Profile
 
 # Create your views here.
 
 def post_list_and_create(request):
-  qs = Post.objects.all()
-  return render(request,'posts/main.html',{'qs':qs})
+  form = PostForm(request.POST or None)
+  # qs = Post.objects.all()
+
+  if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+    if form.is_valid():
+      author = Profile.objects.get(user = request.user)
+      instance = form.save(commit=False)
+      instance.author = author
+      instance.save()
+  context = {
+    'form':form,
+  }
+  return render(request,'posts/main.html',context)
 
 def load_post_data_view(request, num_posts):
   visible = 3
