67523321d1e5eeaf465459cd44a54e81634e8483
Quan Khuong <vkhuong2363@conestogac.on.ca>
2024-03-27 09:47:58 -0400

    Add favicon and disable spinner when there are data

--- dj_ajax/src/posts/static/posts/detail.js
+++ dj_ajax/src/posts/static/posts/detail.js
@@ -1,6 +1,21 @@
 console.log('hekk')
 
 const backBtn = document.getElementById('back-btn')
+const url = window.location.href + "data/"
+const spinnerBox = document.getElementById('spinner-box')
+
 backBtn.addEventListener('click', ()=>{
   history.back()
+})
+
+$.ajax({
+  type:'GET',
+  url: url,
+  success:function(response){
+    console.log(response)
+    spinnerBox.classList.add('not-visible')
+  },
+  error:function(error){
+    console.log(error)
+  }
 })
--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -3,7 +3,8 @@ from .views import (
 post_list_and_create,
 load_post_data_view,
 like_unlike_post,
-post_detail
+post_detail,
+post_detail_data_view,
 
 
 )
@@ -14,4 +15,5 @@ urlpatterns = [path('',post_list_and_create, name = 'main-board'),
                path('like-unlike/',like_unlike_post, name = 'like-unlike'),
                path('data/<int:num_posts>/', load_post_data_view, name = 'posts-data'),
                path('<pk>/',post_detail, name='post-detail'),
+               path('<pk>/data/',post_detail_data_view,name = 'post-detail-data')
                ]
