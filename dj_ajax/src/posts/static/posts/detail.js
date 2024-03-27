50fa72079174083716e4594b8f8ab675553e98b6
Quan Khuong <vkhuong2363@conestogac.on.ca>
2024-03-27 12:34:19 -0400

    Add the content for the detail

--- dj_ajax/src/posts/static/posts/detail.js
+++ dj_ajax/src/posts/static/posts/detail.js
@@ -1,5 +1,6 @@
 console.log('hekk')
 
+const postBox = document.getElementById('post-box')
 const backBtn = document.getElementById('back-btn')
 const updateBtn = document.getElementById('update-btn')
 const deletebtn = document.getElementById('delete-btn')
@@ -24,8 +25,19 @@ $.ajax({
       console.log('the same')
       updateBtn.classList.remove('not-visible')
       deletebtn.classList.remove('not-visible')
-
     }
+
+    const titleEl = document.createElement('h3')
+    titleEl.setAttribute('class', 'mt-3')
+
+    const bodyEl = document.createElement('p')
+    bodyEl.setAttribute('class', 'mt-1')
+
+    titleEl.textContent=data.title
+    bodyEl.textContent=data.body
+
+    postBox.appendChild(titleEl)
+    postBox.appendChild(bodyEl)
 
     spinnerBox.classList.add('not-visible')
   },
