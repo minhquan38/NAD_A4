45806dd6bcd7c4b18148fca4f18eafbc83a706b5
Quan Khuong <vkhuong2363@conestogac.on.ca>
2024-03-27 14:03:11 -0400

    Add the update functionality woth the appropriate alert box

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -5,6 +5,8 @@ load_post_data_view,
 like_unlike_post,
 post_detail,
 post_detail_data_view,
+delete_post,
+update_post,
 
 
 )
@@ -15,5 +17,7 @@ urlpatterns = [path('',post_list_and_create, name = 'main-board'),
                path('like-unlike/',like_unlike_post, name = 'like-unlike'),
                path('data/<int:num_posts>/', load_post_data_view, name = 'posts-data'),
                path('<pk>/',post_detail, name='post-detail'),
+               path('<pk>/update/',update_post, name='post-update'),
+               path('<pk>/delete/',delete_post, name='post-delete'),
                path('<pk>/data/',post_detail_data_view,name = 'post-detail-data')
                ]
