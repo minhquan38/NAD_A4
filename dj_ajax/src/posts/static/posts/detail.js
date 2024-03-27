4bb3861d8cdadf2d9f60fc78853190b4b32bf14e
Quan Khuong <vkhuong2363@conestogac.on.ca>
2024-03-27 13:01:19 -0400

    Add the modal for the delete and update button

--- dj_ajax/src/posts/static/posts/detail.js
+++ dj_ajax/src/posts/static/posts/detail.js
@@ -7,6 +7,10 @@ const deletebtn = document.getElementById('delete-btn')
 const url = window.location.href + "data/"
 const spinnerBox = document.getElementById('spinner-box')
 
+const titleInput = document.getElementById('id_title')
+const bodyInput = document.getElementById('id_body')
+
+
 backBtn.addEventListener('click', ()=>{
   history.back()
 })
@@ -38,6 +42,9 @@ $.ajax({
 
     postBox.appendChild(titleEl)
     postBox.appendChild(bodyEl)
+
+    titleInput.value = data.title
+    bodyInput.value = data.body
 
     spinnerBox.classList.add('not-visible')
   },
